03c4596fd111b2283dbcd959cd92bc74b71873b0
DINGYUN HUANG <94184902+Dingyun-Huang@users.noreply.github.com>
2022-10-10 21:53:52 +0100

    Change the roles of theme compound
    Changed the roles of theme compounds back to ['nesting theme'] to avoid unwanted records.

--- chemdataextractor/parse/cem.py
+++ chemdataextractor/parse/cem.py
@@ -364,7 +364,7 @@ class ThemeChemicalLabelParser(BaseSentenceParser):
 
     def interpret(self, result, start, end):
         # print(etree.tostring(result))
-        roles = [standardize_role(r) for r in result.xpath('./roles/text()')]
+        roles = ['nesting theme']
         for label in result.xpath('./labels/text()'):
             yield self.model(labels=[label], roles=roles)
 
@@ -444,7 +444,7 @@ class ThemeCompoundParser(BaseSentenceParser):
             c = self.model(
                 names=cem_el.xpath('./names/text()'),
                 labels=cem_el.xpath('./labels/text()'),
-                roles=cem_el.xpath('./roles/text()')
+                roles=['nesting theme']
             )
             c.record_method = self.__class__.__name__
             yield c
