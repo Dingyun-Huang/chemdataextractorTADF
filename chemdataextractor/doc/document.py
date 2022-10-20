881d312e15f4568ae5cacf7080112da6c0a4cf1f
DINGYUN HUANG <94184902+Dingyun-Huang@users.noreply.github.com>
2022-10-20 14:05:10 +0100

    Update document.py
    Added a feature to skip given types of elements during parsing, e.g., citations.

--- chemdataextractor/doc/document.py
+++ chemdataextractor/doc/document.py
@@ -77,6 +77,7 @@ class Document(BaseDocument):
         :param list[chemdataextractor.doc.element.BaseElement|string] elements: Elements in this Document.
         :keyword Config config: (Optional) Config file for the Document.
         :keyword list[BaseModel] models: (Optional) Models that the Document should extract data for.
+        :keyword list[chemdataextractor.doc.element.BaseElement subclass] skip_elements: (Optional) Element types to be skipped in parsing
         """
         self._elements = []
         for element in elements:
@@ -98,6 +99,10 @@ class Document(BaseDocument):
             self.models = kwargs['models']
         else:
             self._models = []
+        if 'skip_elements' in kwargs:
+            self.skip_elements = kwargs["skip_elements"]
+        else:
+            self.skip_elements = []
 
         # Sets parameters from configuration file
         for element in elements:
@@ -230,6 +235,10 @@ class Document(BaseDocument):
 
         # Main loop, over all elements in the document
         for i, el in enumerate(self.elements):
+
+            if type(el) in self.skip_elements:
+                continue
+
             log.debug("Element %d, type %s" %(i, str(type(el))))
             last_id_record = None
 
