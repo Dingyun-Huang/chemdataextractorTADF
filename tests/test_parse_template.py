8426cfaf380f0d69ef4cde1292d0429c648fc0eb
DINGYUN HUANG <94184902+Dingyun-Huang@users.noreply.github.com>
2022-10-30 21:00:57 +0000

    Updated template.py and test_parse_template.py
    Fix all bracketing bugs in this script.
    Added a phrase to look at CEM (value phrase).
    Added tests for this.

--- tests/test_parse_template.py
+++ tests/test_parse_template.py
@@ -67,6 +67,12 @@ class TestQuantityModelTemplate(unittest.TestCase):
         expected = b'<root_phrase><raw_value>1100</raw_value><raw_units>K</raw_units><COMMA>,</COMMA><specifier>Curie temperature</specifier><cem_phrase><compound><names>BiFeO3</names></compound></cem_phrase></root_phrase>'
         self.assertEqual(expected, self.parse(s, 'value_specifier_cem_phrase'))
 
+    def test_specifier_cem_bracketed_value(self):
+        s = Sentence(
+            'Curie temperature of BiFeO3 (1100 K)')
+        expected = b'<root_phrase><specifier>Curie temperature</specifier><cem_phrase><compound><names>BiFeO3</names></compound></cem_phrase><raw_value>1100</raw_value><raw_units>K</raw_units></root_phrase>'
+        self.assertEqual(expected, self.parse(s, 'specifier_cem_bracketed_value'))
+
 
 class TestMultiQuantityTemplate(unittest.TestCase):
     maxDiff = None
