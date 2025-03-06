c7e9b992aa57be6c923bc6aee84f8d06108ded10
Dingyun Huang <dh582@cam.ac.uk>
2025-03-06 12:48:42 +0000

    removed debug prints

--- chemdataextractor/doc/text.py
+++ chemdataextractor/doc/text.py
@@ -986,8 +986,7 @@ class Subsentence(Sentence):
         skip_parsers = self.document.skip_parsers if self.document is not None else []
 
         ThemeCompound.local_cems = [chemical_mention.text for chemical_mention in self.parent_sentence.cems]
-        print(self.cems)
-        print(ThemeCompound.local_cems)
+
         for model in self._streamlined_models:
             for parser in model.parsers:
                 if parser in skip_parsers:
